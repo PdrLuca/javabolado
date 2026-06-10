c869b1ff5fcac43050117d49901895175063365c
andré <aluno.den@senaibahia.com.br>
2026-06-10 16:47:55 -0300

    salvar

--- atvddjava/demo/src/main/java/br/com/example/api/DemoApplication.java
+++ atvddjava/demo/src/main/java/br/com/example/api/DemoApplication.java
@@ -1,4 +1,4 @@
-package br.com.example.api.demo;
+package br.com.example.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
